0abc57b01014d41dbb1670bd7463643d09d3902a
Animashaun Mustapha Olanrewaju <106778499+mustapha1607@users.noreply.github.com>
2024-10-07 22:01:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = prompt('a = ?');
+let b = prompt('b = ?');
+
+a = Number(a)
+b = Number(b);
+
+
+let sum = a + b;
+let subtraction = a - b;
+let division = a / b;
+let multiplication = a * b;
+
+document.write('The Sum is:' + sum, ' <br>');
+document.write('The Subtraction gives: ' + subtraction, '<br>');
+document.write('The Division is:' + division, '<br>');
+document.write('The Multiplication of the two numbers gives:' + multiplication, '<br>');
+
+//
